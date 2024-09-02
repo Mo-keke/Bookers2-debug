aef2511d01e8acbeff6b7331682d1e50cbfafa23
Mo-keke <175957589+Mo-keke@users.noreply.github.com>
2024-09-02 16:57:50 +0000

    [Add] Favorite, Comment

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    favorite = Favorite.new(book_id: book.id)
+    favorite.user_id = current_user.id
+    #上記2行はまとめて favorite = current_user.favorites.new(book_id: book.id) とも表記できる
+    favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
+end
